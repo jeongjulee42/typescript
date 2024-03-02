a9e03036166ebba3d775f75d35189aee5334643d
jeongjulee42 <ljj3342@naver.com>
2024-03-02 21:51:56 +0900

    map

--- 0/main.ts
+++ 0/main.ts
@@ -1,31 +1,56 @@
 {
-    const obj = {
-      name: 'ellie',
+    type Video = {
+      title: string;
+      author: string;
     };
-    obj.name; // ellie
-    obj['name']; // ellie
+    // [1, 2].map(item => item * item); // [1, 4]
+  
+    type Optional<T> = {
+      [P in keyof T]?: T[P]; // for...in
+    };
+  
+    type ReadOnly<T> = {
+      readonly [P in keyof T]: T[P];
+    };
+    type VideoOptional = Optional<Video>;
   
     type Animal = {
       name: string;
       age: number;
-      gender: 'male' | 'female';
+    };
+    const animal: Optional<Animal> = {
+      name: 'dog',
+    };
+    animal.name = 'ellie';
+  
+    const video: ReadOnly<Video> = {
+      title: 'hi',
+      author: 'ellie',
     };
   
-    type Name = Animal['name']; // string
-    const text: Name = 'hello';
+    // type VideoOptional = {
+    //   title?: string;
+    //   author?: string;
+    // };
   
-    type Gender = Animal['gender']; //'male' | 'female'
+    // type VideoReadOnly = {
+    //   readonly title: string;
+    //   readonly author: string;
+    // };
   
-    type Keys = keyof Animal; // 'name' | 'age' | 'gender'
-    const key: Keys = 'gender';
-  
-    type Person = {
-      name: string;
-      gender: Animal['gender'];
+    type Nullable<T> = { [P in keyof T]: T[P] | null };
+    const obj2: Nullable<Video> = {
+      title: 'hi',
+      author: null,
     };
-    const person: Person = {
-      name: 'ellie',
-      gender: 'male',
+  
+    type Proxy<T> = {
+      get(): T;
+      set(value: T): void;
+    };
+  
+    type Proxify<T> = {
+      [P in keyof T]: Proxy<T[P]>;
     };
   }
   
