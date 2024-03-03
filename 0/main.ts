9f0196094c085c22ba5d9b26d53b2e169f40a726
jeongjulee42 <ljj3342@naver.com>
2024-03-03 16:18:51 +0900

    condition:

--- 0/main.ts
+++ 0/main.ts
@@ -1,56 +1,21 @@
-{
-    type Video = {
-      title: string;
-      author: string;
-    };
-    // [1, 2].map(item => item * item); // [1, 4]
-  
-    type Optional<T> = {
-      [P in keyof T]?: T[P]; // for...in
-    };
-  
-    type ReadOnly<T> = {
-      readonly [P in keyof T]: T[P];
-    };
-    type VideoOptional = Optional<Video>;
-  
-    type Animal = {
-      name: string;
-      age: number;
-    };
-    const animal: Optional<Animal> = {
-      name: 'dog',
-    };
-    animal.name = 'ellie';
-  
-    const video: ReadOnly<Video> = {
-      title: 'hi',
-      author: 'ellie',
-    };
-  
-    // type VideoOptional = {
-    //   title?: string;
-    //   author?: string;
-    // };
-  
-    // type VideoReadOnly = {
-    //   readonly title: string;
-    //   readonly author: string;
-    // };
-  
-    type Nullable<T> = { [P in keyof T]: T[P] | null };
-    const obj2: Nullable<Video> = {
-      title: 'hi',
-      author: null,
-    };
-  
-    type Proxy<T> = {
-      get(): T;
-      set(value: T): void;
-    };
-  
-    type Proxify<T> = {
-      [P in keyof T]: Proxy<T[P]>;
-    };
-  }
-  
+type Check<T> = T extends string ? boolean : number;
+type Type = Check<string>; // boolean
+
+type TypeName<T> = T extends string
+  ? 'string'
+  : T extends number
+  ? 'number'
+  : T extends boolean
+  ? 'boolean'
+  : T extends undefined
+  ? 'undefined'
+  : T extends Function
+  ? 'function'
+  : 'object';
+
+type T0 = TypeName<string>;
+('string');
+type T1 = TypeName<'a'>;
+('string');
+type T2 = TypeName<() => void>;
+('function');
